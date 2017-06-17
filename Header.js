595126bf4060cdbc528a2078778637f9304a89b1
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-06-17 15:28:19 +0200

    added header input

--- Header.js
+++ Header.js
@@ -1,10 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class Header extends React.Component {
   render() {
     return (
-      <View />
+        <View style={styles.header}>
+            <TextInput
+                placeholder="What you wanna do?"
+                blurOnSubmit={false}
+                returnKeyType="done"
+                style={styles.input}
+            />
+        </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+    header: {
+        paddingHorizontal: 16,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+    input: {
+        flex: 1,
+        height: 50,
+    },
+});
