fdd3f77fa5ea21d4724a689f1a7f804891eadcab
mzolkiewski <zolkiewski.marcin@gmail.com>
2017-06-17 15:18:38 +0200

    added header and footer

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './Header';
+import Footer from './Footer';
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Header />
+        <View style={styles.content}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Changes you make will automatically reload.</Text>
+          <Text>Shake your phone to open the developer menu.</Text>
+        </View>
+        <Footer />
       </View>
     );
   }
@@ -16,8 +23,11 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#eee',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  content: {
+    flex: 1,
   },
 });
